a35845f5b8bbcbbf9ff95ff5f5531d31321ebdb7
Arturs Zigurs <arturszigurs@hotmail.com>
2015-08-16 14:59:06 +0300

    added networking example in main function

--- WifiGreetConsole/WifiGreetConsole/Program.cs
+++ WifiGreetConsole/WifiGreetConsole/Program.cs
@@ -10,6 +10,48 @@ namespace WifiGreetConsole
     {
         static void Main(string[] args)
         {
+            //Create process
+            System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
+
+            //strCommand is path and file name of command to run
+            pProcess.StartInfo.FileName = @"C:\Windows\System32\ARP.EXE";
+
+            //strCommandParameters are parameters to pass to program
+            pProcess.StartInfo.Arguments = "-a";
+
+            pProcess.StartInfo.UseShellExecute = false;
+
+            //Set output of program to be written to process output stream
+            pProcess.StartInfo.RedirectStandardOutput = true;
+
+            //Optional
+            //pProcess.StartInfo.WorkingDirectory = strWorkingDirectory;
+
+            //Start the process
+            pProcess.Start();
+
+            //Get program output
+            string strOutput = pProcess.StandardOutput.ReadToEnd();
+
+            //Wait for process to finish
+            pProcess.WaitForExit();
+
+            Console.WriteLine(strOutput);
+            Console.ReadKey();
+
+            //// Start the child process.
+            //Process p = new Process();
+            //// Redirect the output stream of the child process.
+            //p.StartInfo.UseShellExecute = false;
+            //p.StartInfo.RedirectStandardOutput = true;
+            //p.StartInfo.FileName = "YOURBATCHFILE.bat";
+            //p.Start();
+            //// Do not wait for the child process to exit before
+            //// reading to the end of its redirected stream.
+            //// p.WaitForExit();
+            //// Read the output stream first and then wait.
+            //string output = p.StandardOutput.ReadToEnd();
+            //p.WaitForExit();
         }
     }
 }
