cb16303ccf237ccbdc68ece5ca3d63733cd74701
Arturs Zigurs <arturszigurs@gmail.com>
2015-08-22 10:06:16 +0300

    Web Api project with basic functionality for alarms

---
+++ WifiGreetConsole/WebApiServer/Controllers/AlarmsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiServer.Models;
+
+namespace WebApiServer.Controllers
+{
+    public class AlarmsController : ApiController
+    {
+        Alarm[] alarmsList = new Alarm[]
+        {
+            new Alarm { isRepeatable = true, AlarmText = "Alarm one" },
+            new Alarm { isRepeatable = true, AlarmText = "Alarm two"}
+        };
+
+        [Route("alarms/alarmsforuser")]
+        public IEnumerable<Alarm> GetAllAlarms()
+        {
+            return alarmsList;
+        }
+
+        //[Route("customers/{customerId}/orders")]
+        [Route("alarms/getalarm/{Id}")]
+        public IHttpActionResult GetAlarms(int Id)
+        {
+            var alarm = alarmsList.FirstOrDefault((p) => p.id == Id);
+            if (alarm == null)
+            {
+                return NotFound();
+            }
+            return Ok(alarm);
+        }
+    }
+}
---
+++ WifiGreetConsole/WebApiServer/Models/Alarm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiServer.Models
+{
+    public class Alarm
+    {
+        public int id { get; set; }
+        public bool isRepeatable { get; set; }
+        public DateTime AlarmTime { get; set; }
+        public int SnoozeCount { get; set; }
+        public bool[] weekdays { get; set; }
+        public string AlarmTuneLocation { get; set; }
+        public string AlarmText { get; set; }
+        public string Owner { get; set; }
+        public DateTime AlarmCreated { get; set; }
+    }
+}
