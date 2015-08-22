3d80f240c8e89edc63d88563b6f8e459d58801cf
Arturs Zigurs <arturszigurs@gmail.com>
2015-08-22 12:48:45 +0300

    #12 basic interfaces implemented. updated api descriptionpage index.html

--- WifiGreetConsole/WebApiServer/Controllers/AlarmsController.cs
+++ WifiGreetConsole/WebApiServer/Controllers/AlarmsController.cs
@@ -10,21 +10,24 @@ namespace WebApiServer.Controllers
 {
     public class AlarmsController : ApiController
     {
-        Alarm[] alarmsList = new Alarm[]
+        /*Alarm[] alarmsList = new Alarm[]
         {
             new Alarm { isRepeatable = true, AlarmText = "Alarm one" },
             new Alarm { isRepeatable = true, AlarmText = "Alarm two"}
-        };
+        };*/
+
+        List<Alarm> alarmsList = new List<Alarm>();
+        
 
         [Route("alarms/alarmsforuser")]
         public IEnumerable<Alarm> GetAllAlarms()
         {
+            //return alarmsList;
             return alarmsList;
         }
 
-        //[Route("customers/{customerId}/orders")]
         [Route("alarms/getalarm/{Id}")]
-        public IHttpActionResult GetAlarms(int Id)
+        public IHttpActionResult GetAlarm(int Id)
         {
             var alarm = alarmsList.FirstOrDefault((p) => p.id == Id);
             if (alarm == null)
@@ -32,6 +35,26 @@ namespace WebApiServer.Controllers
                 return NotFound();
             }
             return Ok(alarm);
+        }
+
+        [Route("alarms/addalarm")]
+        public IHttpActionResult AlarmAdd(int Id)
+        {
+            //for now creates alarm just to see that interface works
+            Alarm tmpAllarm = new Alarm();
+            tmpAllarm.AlarmCreated = DateTime.Now;
+            tmpAllarm.id = 67;
+            tmpAllarm.AlarmTuneLocation = "media/file/location/here/filename.txt";
+            tmpAllarm.isRepeatable = true;
+            tmpAllarm.SnoozeCount = 2;
+            tmpAllarm.AlarmText = "Sample Alarm";
+            tmpAllarm.Owner = "arturszigurs";
+            if (tmpAllarm.Owner == "zigurs93")
+                {
+                return BadRequest("Alarm could not be added properly");
+                }
+            alarmsList.Add(tmpAllarm);
+            return Ok("Alarm succesfully added");        
         }
     }
 }
