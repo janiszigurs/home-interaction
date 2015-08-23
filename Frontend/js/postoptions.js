d0d77df1c63f04c266e54fcdb95046b4603fcbcd
arnsos <arnisozo13@gmail.com>
2015-08-23 18:47:15 +0100

    Fixed #19 Alarms can be added

---
+++ Frontend/js/postoptions.js
@@ -0,0 +1,60 @@
+var days = [];
+var ir = "";
+
+function Postarticle() 
+{	
+	if ($("#mon").prop('checked')) {
+		days += "1";
+	} else {
+		days += "0";
+	}
+	if ($("#tue").prop('checked')) {
+		days += "1";
+	} else {
+		days += "0";
+	}
+	if ($("#wed").prop('checked')) {
+		days += "1";
+	} else {
+		days += "0";
+	}
+	if ($("#thu").prop('checked')) {
+		days += "1";
+	} else {
+		days += "0";
+	}
+	if ($("#fri").prop('checked')) {
+		days += "1";
+	} else {
+		days += "0";
+	}
+	if ($("#sat").prop('checked')) {
+		days += "1";
+	} else {
+		days += "0";
+	}
+	if ($("#sun").prop('checked')) {
+		days += "1";
+	} else {
+		days += "0";
+	}
+	
+	if ($("#ir").val('on')) {
+		ir = "true";
+	} else {
+		ir = "false";
+	}
+	
+    $.ajax({
+    url: "http://84.237.250.21:55000/alarms/add?days="+encodeURIComponent(days)+"&tunelocation="+encodeURIComponent($("#tunelocation").val())+"&ir="+ir+"&snoozecount="+encodeURIComponent($("#snoozecount").val())+"&alarmtext="+encodeURIComponent($("#alarmtext").val())+"&owner="+encodeURIComponent($("#owner").val())+"&hh="+encodeURIComponent($("#hh").val())+"&mm="+encodeURIComponent($("#mm").val()),
+    dataType: "json",
+    success: function (response) {
+        console.log('Success response: ' + JSON.stringify(response));
+    },
+    fail: function (response) {
+        console.log('Fail response: ' + JSON.stringify(response));
+    }
+});
+
+
+}
